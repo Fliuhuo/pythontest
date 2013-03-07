9132275f424988a416d79c022b9d4e1c04a62898
lty <F806423398@gmail.com>
2013-03-07 17:17:00 +0800

    make beautiful
    maybe

--- player.py
+++ player.py
@@ -15,11 +15,15 @@ class Player:
     def make(self):
         self.tempnum = 0
         for i in range(len(self.preferred_attrs)):
-            if self.preferred_attrs[i] == True:
+	
+            print "self.num is ",self.num
+            if self.preferred_attrs[i]:
                 if self.num > 35:
                     self.tempnum = 15;
                     num = random.randint(0,self.tempnum)
-                    self.num = self.num - num - 20;
+	            print "random num is",num                    
+
+		    self.num = self.num - num - 20;
                     self.attributes[i] = 60+num+20
                 else:
                     self.tempnum = self.num;
@@ -28,7 +32,7 @@ class Player:
                     self.attributes[i] = 60+num
                 
         for i in range(len(self.preferred_attrs)):
-            if self.preferred_attrs[i] == False:
+            if not self.preferred_attrs[i]:
                 if self.num > 35:
                     self.tempnum = 35;
                     num = random.randint(0,self.tempnum)
@@ -38,17 +42,19 @@ class Player:
                     self.tempnum = self.num;
                     num = random.randint(0,self.tempnum)
                     self.num = self.num - num;
-                    self.attributes[i] =  60+num
+                    self.attributes[i] =  60 + num
 
         if self.num > 0:
-            for attr in  self.attributes:
-                if num + attr < 95:
-                    attr = attr + num
+            print "self.num is not used ",self.num
+            for i in  range(len(self.attributes)):
+                if self. num + self.attributes[i] < 95:
+                    self.attributes[i] = self.attributes[i] + self.num
+                    print self.attributes[i]
                     return
                 else:
-                    tempnum = attr+num
-                    num = tempnum - 95
-                    attr = 95
+                    tempnum = self.attributes[i] + self.num
+                    self.num = tempnum - 95
+                    self.attributes[i] = 95
                 
     def __str__(self):
         return self.skill + '   ' + ', '.join(str(attr) for attr in self.attributes)
@@ -56,18 +62,13 @@ class Player:
         #return str(self.attributes)
         
           
-def test():
-    skill = 'test'
-    preferred_attrs = [ True, True, False, True, False]
-    if len(sys.argv) >= 3:
-       skill = sys.argv[1]
-       preferred_attrs=sys.argv[2]
-
+def test(skill,preferred_attrs):
     p = Player(skill, preferred_attrs)
+    print "preferred_attrs is ", str(preferred_attrs)
     p.make()
     print p
               
 if __name__ == '__main__':
-    test()
+    test(skill,list1)
 
   
