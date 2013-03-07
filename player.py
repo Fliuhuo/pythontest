368cf4605e99eb93eea95f447aacad9326f56824
lty <F806423398@gmail.com>
2013-03-07 13:22:03 +0800

    Revert "Revert "try to make beautiful""
    This reverts commit 2d0bcdd9e7e3b5e66d34f74e101481a45d104959.

--- player.py
+++ player.py
@@ -7,58 +7,65 @@ import sys
 
 class Player:
     def __init__(self, skill, preferred_attrs=[]):
-        self.attribute = [60,60,60,60,60]
+        self.attributes = [60,60,60,60,60]
         self.num = 80
         self.skill = skill
         self.preferred_attrs = preferred_attrs
         
     def make(self):
         self.tempnum = 0
-        for i in range(5):
-            if(self.preferred_attrs[i]  == 1):
+        for i in range(len(self.preferred_attrs)):
+            if self.preferred_attrs[i] == True:
                 if self.num > 35:
                     self.tempnum = 15;
                     num = random.randint(0,self.tempnum)
                     self.num = self.num - num - 20;
-                    self.attribute[i] = 60+num+20
+                    self.attributes[i] = 60+num+20
                 else:
                     self.tempnum = self.num;
                     num = random.randint(0,self.tempnum)
                     self.num = self.num - num;
-                    self.attribute[i] = 60+num
-
+                    self.attributes[i] = 60+num
                 
-                
-        for i in range(5):
-            if(self.preferred_attrs[i]  == 0):
+        for i in range(len(self.preferred_attrs)):
+            if self.preferred_attrs[i] == False:
                 if self.num > 35:
                     self.tempnum = 35;
                     num = random.randint(0,self.tempnum)
                     self.num = self.num - num;
-                    self.attribute[i] =  60+num
+                    self.attributes[i] =  60+num
                 else:
                     self.tempnum = self.num;
                     num = random.randint(0,self.tempnum)
                     self.num = self.num - num;
-                    self.attribute[i] =  60+num
+                    self.attributes[i] =  60+num
+
+        if self.num > 0:
+            for attr in  self.attributes:
+                if num + attr < 95:
+                    attr = attr + num
+                    return
+                else:
+                    tempnum = attr+num
+                    num = tempnum - 95
+                    attr = 95
                 
-                
-    def printself(self):
-        for i in range(5):
-            print self.skill
-            print self.attribute[i];
-          
+    def __str__(self):
+        return self.skill + '   ' + ', '.join(str(attr) for attr in self.attributes)
+        #return ', '.join(self.attributes)
+        #return str(self.attributes)
+        
           
 def test():
     skill = 'test'
-    preferred_attrs = [ 0, 1, 0, 1, 0]
+    preferred_attrs = [ True, True, False, True, False]
     if len(sys.argv) >= 3:
        skill = sys.argv[1]
        preferred_attrs=sys.argv[2]
 
     p = Player(skill, preferred_attrs)
     p.make()
-    p.printself(); 
+    print p
               
 if __name__ == '__main__':
     test()
